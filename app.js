89f6b7bb46f3a7bbca61d9f8ee9baad56991af3a
Makhmudov Abdulqodir <makhmudovabdulqodir@Makhmudovs-MacBook-Air.local>
2025-08-21 11:27:06 +0500

    first

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 let clockdiv = document.getElementById('clock');
-function showTime(){
+
+function showTime() {
     let now = new Date();
     let hours = now.getHours();
     let minutes = now.getMinutes();
     let seconds = now.getSeconds();
+
+    // Oldida 0 qo'shish
+    if (hours < 10) hours = '0' + hours;
+    if (minutes < 10) minutes = '0' + minutes;
+    if (seconds < 10) seconds = '0' + seconds;
+
     clockdiv.textContent = hours + ':' + minutes + ':' + seconds;
 }
-setInterval(showTime,1000);
+
+setInterval(showTime, 1000);
 showTime();
-
-if(minutes < 10){
-    minutes = '0' + minutes
-} 
-if(seconds < 10){
-    seconds = '0' + seconds
-} 
-if(hours < 10){
-    hours = '0' + hours
-} 
-
